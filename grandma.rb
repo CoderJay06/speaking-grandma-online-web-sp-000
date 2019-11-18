2c19de186ae09e62437e19bb0e342d18cba81a8f
Jason J Cruz <jaycru06@yahoo.com>
2019-11-18 01:25:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase.downcase || phrase.capitalize
+  elsif !phrase.upcase
     return 'HUH?! SPEAK UP, SONNY!'
   else
      return 'NO, NOT SINCE 1938!'
